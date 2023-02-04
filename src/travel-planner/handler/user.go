8417ed2a03643b172e4922a2098374ab8e2b6039
Zhenlin Wang <djj87018507@gmail.com>
2023-02-04 00:18:49 -0500

    fix token error

--- src/travel-planner/handler/user.go
+++ src/travel-planner/handler/user.go
@@ -73,7 +73,7 @@ func SigninHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	// check user exist or not
-	exist, err := service.CheckUser(user.Email, user.Password)
+	exist, idString, err := service.CheckUser(user.Email, user.Password)
 	if err != nil {
 		http.Error(w, "Failed to read user from database", http.StatusInternalServerError)
 		fmt.Printf("Failed to read user from database %v\n", err)
@@ -88,8 +88,9 @@ func SigninHandler(w http.ResponseWriter, r *http.Request) {
 
 	// generate token
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"email": user.Email,
-		"exp":   time.Now().Add(time.Hour * 24).Unix(),
+		"email":          user.Email,
+		"user_id_string": idString,
+		"exp":            time.Now().Add(time.Hour * 24).Unix(),
 	})
 
 	// sign and get the complete encoded token as a string using the secret
@@ -122,8 +123,8 @@ func GetUserHandler(w http.ResponseWriter, r *http.Request) {
 
 	intId, _ := strconv.ParseInt(id, 0, 64)
 	fmt.Printf("intId : %v\n", intId)
-	pasedId := uint32(intId)
-	user, err := service.CheckUserInfo(uint32(pasedId))
+	parsedId := uint32(intId)
+	user, err := service.CheckUserInfo(uint32(parsedId))
 
 	if err != nil {
 		http.Error(w, "Failed to read user info from backend", http.StatusInternalServerError)
